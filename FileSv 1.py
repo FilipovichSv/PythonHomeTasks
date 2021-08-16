c032fa7a8142c6f01998eef64d50063aed3d3fcd
FilipovichSv <60697031+FilipovichSv@users.noreply.github.com>
2021-08-16 21:43:45 +0300

    Add files via upload

---
+++ FileSv 1.py
@@ -0,0 +1,8 @@
+import random
+n=int(input("10"))
+                         A=[]
+                         for i in range (n):
+                             a=random.random()
+                             A.append(a)
+                             for i in range (n):
+                                 print (A([i]))
