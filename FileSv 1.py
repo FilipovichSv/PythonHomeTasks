70efc977af94e513a44d00fc4c3d2e96778f807a
FilipovichSv <60697031+FilipovichSv@users.noreply.github.com>
2021-08-17 20:10:18 +0300

    Delete FileSv 1.py

--- FileSv 1.py
+++
@@ -1,8 +0,0 @@
-import random
-n=int(input("10"))
-                         A=[]
-                         for i in range (n):
-                             a=random.random()
-                             A.append(a)
-                             for i in range (n):
-                                 print (A([i]))
